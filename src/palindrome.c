c35f3afda1be4331554e15bd6e5dbf49092353a2
20373384 <395549131@qq.com>
2022-03-13 22:38:37 +0800

    exercise 0.1

--- src/palindrome.c
+++ src/palindrome.c
@@ -3,18 +3,24 @@ int main()
 {
 	int n;
 	scanf("%d",&n);
+	int k = 1;
+	int a[6];
+	int i;
+	if (n >= 10) k++;
+	if (n >= 100) k++;
+	if (n >= 1000) k++;
+	if (n >= 10000) k++;
+	for (i = 0; i < k; i++) {
+		a[i] = n % 10;
+		n /= 10;
+	}
+	for (i = 0; i < k; i++)
+		if (a[i] != a[k - i - 1]) break;
 
 
 
 
-
-
-
-
-
-
-
-	if(){
+	if(i == k){
 		printf("Y");
 	}else{
 		printf("N");
