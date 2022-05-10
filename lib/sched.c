93ec4bf040b7b42ca16d29f8cd775811b3b68240
20373384 <395549131@qq.com>
2022-05-10 22:37:23 +0800

    for lab4

--- lib/sched.c
+++ lib/sched.c
@@ -16,7 +16,8 @@ void sched_yield(void)
 {
     static int count = 0; // remaining time slices of current env
     static int point = 0; // current env_sched_list index
-	static struct Env *e = NULL;    
+	static struct Env *e = NULL;
+	//env_run(LIST_FIRST(env_sched_list));    
     /*  hint:
      *  1. if (count==0), insert `e` into `env_sched_list[1-point]`
      *     using LIST_REMOVE and LIST_INSERT_TAIL.
