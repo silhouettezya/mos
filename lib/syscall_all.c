8746c566df7b06afbeff8d81b4b16b97c49c664f
20373384 <395549131@qq.com>
2022-05-19 00:15:48 +0800

    Initial commit

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -67,6 +67,10 @@ u_int sys_getenvid(void)
 /*** exercise 4.6 ***/
 void sys_yield(void)
 {
+	bcopy((void *) KERNEL_SP - sizeof(struct Trapframe),
+          (void *) TIMESTACK - sizeof(struct Trapframe),
+          sizeof(struct Trapframe));
+    sched_yield();
 }
 
 /* Overview:
@@ -147,6 +151,25 @@ int sys_mem_alloc(int sysno, u_int envid, u_int va, u_int perm)
 	int ret;
 	ret = 0;
 
+	if (!(perm & PTE_V) || (perm & PTE_COW)) {
+		return -E_INVAL;
+	}	
+    if (va >= UTOP) {
+		return -E_INVAL;
+	}
+    ret = envid2env(envid, &env, 1);
+    if (ret < 0) {
+		return ret;
+	}
+    ret = page_alloc(&ppage);
+    if (ret < 0) {
+		return ret;
+	}
+    ret = page_insert(env->env_pgdir, ppage, va, perm);
+    if (ret < 0) {
+		return ret;
+	}	
+    return 0;
 }
 
 /* Overview:
@@ -179,8 +202,35 @@ int sys_mem_map(int sysno, u_int srcid, u_int srcva, u_int dstid, u_int dstva,
 	round_dstva = ROUNDDOWN(dstva, BY2PG);
 
     //your code here
+	if (srcva >= UTOP || dstva >= UTOP) {
+		return -E_INVAL;
+	}
+    if (!(perm & PTE_V)) {
+		return -E_INVAL;
+	}
 
-	return ret;
+    if ((ret = envid2env(srcid, &srcenv, 0)) < 0) {
+		return ret;
+	}
+	if ((ret = envid2env(dstid, &dstenv, 0)) < 0) {
+		return ret;
+	}
+
+    ppage = page_lookup(srcenv->env_pgdir, round_srcva, &ppte);
+    if (ppage == NULL) {
+		return -E_INVAL;
+	}
+
+    if (!(*ppte & PTE_R) && (perm & PTE_R)) {
+		return -E_INVAL;
+	}
+
+    if ((ret = page_insert(dstenv->env_pgdir, ppage, round_dstva, perm)) < 0) {
+		return ret;
+	}
+
+    return 0;
+	
 }
 
 /* Overview:
@@ -198,6 +248,11 @@ int sys_mem_unmap(int sysno, u_int envid, u_int va)
 	// Your code here.
 	int ret;
 	struct Env *env;
+
+	if (va >= UTOP) return -E_INVAL;
+    if ((ret = envid2env(envid, &env, 1)) < 0) return ret;
+
+    page_remove(env->env_pgdir, va);
 
 	return ret;
 	//	panic("sys_mem_unmap not implemented");
@@ -299,6 +354,11 @@ void sys_panic(int sysno, char *msg)
 /*** exercise 4.7 ***/
 void sys_ipc_recv(int sysno, u_int dstva)
 {
+	if (dstva >= UTOP) return;
+    curenv->env_ipc_recving = 1;
+    curenv->env_ipc_dstva = dstva;
+    curenv->env_status = ENV_NOT_RUNNABLE;
+    sys_yield();
 }
 
 /* Overview:
@@ -326,6 +386,22 @@ int sys_ipc_can_send(int sysno, u_int envid, u_int value, u_int srcva,
 	int r;
 	struct Env *e;
 	struct Page *p;
+	
+	if (srcva >= UTOP) return -E_INVAL;
+    if ((r = envid2env(envid, &e, 0)) < 0) return r;
+    if (e->env_ipc_recving == 0) return -E_IPC_NOT_RECV;
+    e->env_ipc_value = value;
+    e->env_ipc_from = curenv->env_id;
+    e->env_ipc_perm = perm;
+    e->env_ipc_recving = 0;
+    e->env_status = ENV_RUNNABLE;
+
+    if (srcva != 0) {
+        Pte *pte;
+        p = page_lookup(curenv->env_pgdir, srcva, &pte);
+        if (p == NULL) return -E_INVAL;
+        page_insert(e->env_pgdir, p, e->env_ipc_dstva, perm);
+    }
 
 	return 0;
 }
